fa9a49e8e2809c868d00f8e11b1d9d033332640e
david <david@davids-macbook-pro.local>
2020-05-21 20:42:14 +0200

    add build

---
+++ build/precache-manifest.13f97110cb885cb56240277d01ff81dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdbec230aa4e57065de06bff59363f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07500959454325ee48a7",
+    "url": "./static/css/main.2b31dc81.chunk.css"
+  },
+  {
+    "revision": "a673596995d27b0afb28",
+    "url": "./static/js/2.ab4bcd1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ab4bcd1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07500959454325ee48a7",
+    "url": "./static/js/main.9c6d1082.chunk.js"
+  },
+  {
+    "revision": "ad5d219fdb4a093b02fe",
+    "url": "./static/js/runtime-main.9293fd4f.js"
+  }
+]);
